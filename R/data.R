ab2faeb771d305d8c0ce5446d33f5eb1dd35c5db
Ernest Guevarra <ernest@validmeasures.org>
2019-05-19 19:55:24 +0100

    create functions

---
+++ R/data.R
@@ -0,0 +1,17 @@
+################################################################################
+#
+#' Description of variables that occurs in the Global Nutrition Report (GNR)
+#' dataset
+#'
+#' @format A data.frame with 709 rows and 3 columns:
+#' \describe{
+#'     \item{\code{section}}{Section of the report}
+#'     \item{\code{variable}}{Data variable}
+#'     \item{\code{description}}{Description of variable}
+#' }
+#'
+#' @source \href{https://globalnutritionreport.org}{Global Nutrition Report}
+#'
+#
+################################################################################
+"indicator_descriptions"
---
+++ R/gnr.R
@@ -0,0 +1,17 @@
+################################################################################
+#
+#' gnr: R companion to the Global Nutrition Report
+#'
+#' The \code{gnr} package is a companion package to the Global Nutrition Report
+#' primarily for accessing the data used in the report and to reproduce the data
+#' visualisation produced in the report. This package contains functions that
+#' allow users to interrogate the same data and produce customised charts and
+#' other visualisation products.
+#'
+#' @docType package
+#' @name gnr
+#'
+#'
+#
+################################################################################
+NULL
