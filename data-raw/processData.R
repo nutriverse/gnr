9f823ac0e6dda88207039ae661f1b8e41a0d3657
Ernest Guevarra <ernest@validmeasures.org>
2019-05-19 19:55:43 +0100

    create dataset

--- data-raw/processData.R
+++ data-raw/processData.R
@@ -6,8 +6,18 @@
 
 if(!require(readxl)) install.packages("readxl")
 
-
-
-
 url <- "https://globalnutritionreport.org/documents/348/2018_Global_Nutrition_Report_Dataset_F0lDqIh.xlsx"
+
+destFile <- tempfile()
+
+download.file(url = url, destfile = destFile)
+
+indicator_descriptions <- read_xlsx(path = destFile,
+                                    sheet = excel_sheets(destFile)[2],
+                                    range = "A5:C714")
+
+usethis::use_data(indicator_descriptions, overwrite = TRUE)
+
+
+
 
