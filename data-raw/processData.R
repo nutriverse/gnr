647307f564dfc0e29898851084d014e45f97dea9
Ernest Guevarra <ernest@validmeasures.org>
2019-05-19 16:22:41 +0100

    initial commit

---
+++ data-raw/processData.R
@@ -0,0 +1,13 @@
+################################################################################
+#
+# Load libraries
+#
+################################################################################
+
+if(!require(readxl)) install.packages("readxl")
+
+
+
+
+url <- "https://globalnutritionreport.org/documents/348/2018_Global_Nutrition_Report_Dataset_F0lDqIh.xlsx"
+
---
+++ inst/createHexSticker.R
@@ -0,0 +1,7 @@
+library(hexSticker)
+
+sticker("inst/figures/gnr.png",
+        s_x = 1, s_y = 1, s_width = 0.5,
+        package = "", p_color = "#475C6D", p_y = 1.5,
+        h_size = 1.5, h_color = "#DE5D09", h_fill = "#FFFFFF",
+        filename = "man/figures/gnr.png")
